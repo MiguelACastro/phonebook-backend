0b88ecd9e4334b168e932281a822748480badafa
Miguel Castro <mcastro_23@alu.uabcs.mx>
2026-03-29 21:39:45 -0700

    3.19 and 3.20 Add name and number validation

--- index.js
+++ index.js
@@ -48,17 +48,6 @@ app.get('/api/persons/:id', (request, response, next) => {
 app.post('/api/persons', (request, response, next) => {
     const body = request.body
     
-    if (!body.name || !body.number) {
-        return response.status(400).json({ 
-            error: 'content missing' 
-        })
-    }
-
-    // if(persons.find(p => p.name === body.name)) {
-    //     return response.status(400).json({ 
-    //         error: 'name must be unique' 
-    //     })    
-    // }
     const person = new Person({
         name: body.name,
         number: body.number
@@ -103,6 +92,8 @@ const errorHandler = (error, request, response, next) => {
 
     if(error.name === 'CastError') {
         return response.status(400).send({ error: 'Malformatted id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
