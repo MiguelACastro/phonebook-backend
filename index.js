b2bed0f52a2fb35dc936011735629ea5d2a32eb9
Miguel Castro <mcastro_23@alu.uabcs.mx>
2026-03-29 20:50:33 -0700

    3.18 Update GET api/notes/:id and info routes

--- index.js
+++ index.js
@@ -15,33 +15,14 @@ app.use(express.json())
 app.use(express.static('dist'))
 app.use(morgan('data'))
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-app.get('/info', (request, response) => {
-    response.send(`
-        <p>Phonebook has info for ${persons.length} people </p>
-        <p>${new Date()}<p/>`)
+app.get('/info', (request, response, next) => {
+    Person.countDocuments()
+        .then(count => {
+            response.send(`
+            <p>Phonebook has info for ${count} people </p>
+            <p>${new Date()}<p/>`)
+        })
+        .catch(error => next(error))
 })
 
 app.get('/api/persons', (request, response, next) => {
@@ -52,13 +33,16 @@ app.get('/api/persons', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const person = persons.find(p => p.id === request.params.id)
-    if(person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if(person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
